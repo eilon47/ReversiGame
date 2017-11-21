8deecae24199b2f54e4a51c7270a708f085904f7
eilon47 <eilon47@gmail.com>
2017-11-21 15:50:32 +0200

    Added Rules and Changed Play

---
+++ RegularRules.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elon on 21/11/17.
+//
+
+#include "RegularRules.h"
---
+++ RegularRules.h
@@ -0,0 +1,12 @@
+//
+// Created by elon on 21/11/17.
+//
+
+#ifndef REVERSIGAME_REGULARRULES_H
+#define REVERSIGAME_REGULARRULES_H
+
+class RegularRules {
+
+};
+
+#endif //REVERSIGAME_REGULARRULES_H
